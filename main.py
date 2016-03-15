719b3cbe0ee47c8d0f1b2a2d801824279db3a6d9
kevinfitzpatrick <mastery414@gmail.com>
2016-03-15 15:33:06 +0000

    LOOP FINISHED!

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ import time
 
 Scrape.create_Database()
 
+def single_run():
+    data = Scrape.scrape_Info()
+    Scrape.into_File(data, 'testscrape.csv')
+    Scrape.into_Database(data)
 
-data = Scrape.scrape_Info()
-Scrape.into_File(data, 'testscrape.csv')
-Scrape.into_Database(data)
 
-#while True:
-    #Scrape the data from the API.
-    #data = Scrape.scrape_Info()
-    #if data:
-        #Into the .csv
-        #Scrape.into_File(data, 'testscrape.csv')
-        #Into the database
-        #Scrape.into_Database(data)
-    #time.sleep(120)
+def full_run():
+    while True:
+        Scrape the data from the API.
+        data = Scrape.scrape_Info()
+        if data:
+            Into the .csv
+            Scrape.into_File(data, 'fullscrape.csv')
+            Into the database
+            Scrape.into_Database(data)
+        #
+        time.sleep(300)
+
+
+single_run()
+#full_run()
